62dea30a587e8a5b2c05c8dd149633e943be83b7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2012-02-02 15:20:15 +0100

    Add project files for 2.0, 4.0 and Silverlight. Add a few types that Silverlight lacks to reduce the amount of ifdefs required

---
+++ Projects/System.Collections.Specialized/ListDictionary.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace System.Collections.Specialized {
+	public class ListDictionary : Dictionary<object, object> {
+
+	}
+}
+
---
+++ Projects/System.Collections/Hashtable.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+namespace System.Collections {
+	public class Hashtable : Dictionary<object, object> {
+
+	}
+}
+
